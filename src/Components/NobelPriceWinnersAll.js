214f20c88ae8efbbaaa82e9d1fd10a31def4095e
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-10-29 11:29:22 +0530

    feat:added filter option

--- src/Components/NobelPriceWinnersAll.js
+++ src/Components/NobelPriceWinnersAll.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNobelContext } from "../Context/NobelPriceContext";
 import IndividualNobelPrize from "./IndividualNobelPrize";
 import "./app.css";
+import { filterData, getYears } from "../utils/NobelUtils";
+import Category from "./Category";
 function NobelPriceWinnersAll() {
   const {
-    state: { loading, nobelData, filterByCategoryNames },
+    state: { loading, nobelData },
     nobelDispatch,
   } = useNobelContext();
+
+  const [filterByYear, setFilterByYear] = useState("default");
+  const [filterByCategory, setFilterByCategory] = useState("default");
+
   return (
     <div>
+      <Category
+        data={nobelData}
+        setFilterByYear={setFilterByYear}
+        setFilterByCategory={setFilterByCategory}
+      />
       <div className="grid-container">
-        {nobelData.map((ele) => (
+        {filterData(filterByYear, filterByCategory, nobelData).map((ele) => (
           <IndividualNobelPrize data={ele} />
         ))}
       </div>
-      filterByCategoryNames
     </div>
   );
 }
--- src/utils/NobelUtils.js
+++ src/utils/NobelUtils.js
@@ -9,10 +9,41 @@ export const FetchData = async (url) => {
   }
 };
 
-const getYears = () => {
+export const getYears = () => {
   let result = [];
   for (let i = 1900; i <= 2018; i++) {
     result.push(i);
   }
+  return result;
+};
+
+export const getCategories = (data) => {
+  let result = data.map((ele) => ele.category);
+  return result.filter((ele, i) => result.indexOf(ele) === i);
+};
+
+export const filterData = (filterCategory1, filterCategory2, data) => {
+  let result = JSON.parse(JSON.stringify(data));
+  console.log(filterCategory1);
+  if (filterCategory1 === "default" && filterCategory2 === "default") {
+    return result;
+  }
+  if (filterCategory1 !== "default") {
+    result = result.filter((ele) => {
+      return (
+        ele.year.toString().toLowerCase() ===
+        filterCategory1.toString().toLowerCase()
+      );
+    });
+  }
+  if (filterCategory2 !== "default") {
+    result = result.filter((ele) => {
+      return (
+        ele.category.toString().toLowerCase() ===
+        filterCategory2.toString().toLowerCase()
+      );
+    });
+  }
+
   return result;
 };
